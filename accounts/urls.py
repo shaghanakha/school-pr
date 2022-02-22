c82ee30d63eb669b660a16f36fd6819d38273048
shaghanakha <shaaanaaa333@gmail.com>
2022-02-22 12:43:53 +0330

    update feb20-4

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 from .views import TeacherRegistration, LogoutAPIView, Profile
 
 urlpatterns = [
-    path('login/', obtain_auth_token),
-    path('logout/', LogoutAPIView.as_view()),
-    path('register/teacher/', TeacherRegistration.as_view()),
+    path('login/', obtain_auth_token, name="login"),
+    path('logout/', LogoutAPIView.as_view(), name="logout"),
+    path('register/teacher/', TeacherRegistration.as_view(), name="t_register"),
     path('profile/<str:username>', Profile.as_view(), name="profile"),
 ]
